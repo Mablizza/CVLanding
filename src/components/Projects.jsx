6bfb74f9aa377977957eb4f6942e2fb5e47581b1
Julian Arias <mablizza@gmail.com>
2025-03-14 23:22:05 -0600

    Adding Links
    Links to Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,18 +5,25 @@ import QuickGameSetupLT from "/src/assets/QuickGameSetupLT.png"
 export default function Projects(){
     const projectsArr = [
         {title: "Dunu506",
-        tag: "Capstone",
+        tag: "#Capstone",
         icon: Dunu506,
-        description: "The project that started it all... Co-founded this project a couple years ago before I knew how to program. It inspired a pivotal career change which still pushes me today to keep learning about technologies such as AWS, Ruby on Rails, PostgreSQL or PHP. It gave me the motivation to switch to a programming career 🎉. "},
+        description: "The project that started it all... Co-founded this project a couple years ago before I knew how to program. It inspired a pivotal career change which still pushes me today to keep learning about technologies such as AWS, Ruby on Rails, PostgreSQL or PHP. It gave me the motivation to switch to a programming career 🎉. ",
+        url: "https://www.dunu506.com"},
         {title: "BoardGame Quick Setup",
-        tag: "MVP",
+        tag: "#MVP",
         icon: QuickGameSetupLT,
-        description: "A MVP of a useful niche app for boardgamers. A small group of my friends enjoy playing boardgames. Overtime we have built a rather large inventory of boardgames for which we thought having an app with quick setup guides would be useful. This is a prototype of that idea."},
+        description: "A MVP of a useful niche app for boardgamers. A small group of my friends enjoy playing boardgames. Overtime we have built a rather large inventory of boardgames for which we thought having an app with quick setup guides would be useful. This is a prototype of that idea.",
+        url: "https://boardgame-quicksetup-app.pages.dev/"},
         {title: "Random Number Generator",
-        tag: "Hobby",
+        tag: "#Hobby",
         icon: RNGLT,
-        description: "I have a 5year old daughter. Using her ideas I created a 'Random Number Generator' game to help her practice numbers from 0-100. She helped me choose the gif animations as well suggest we add a visual mini-game such as the tower jumble game."},
+        description: "I have a 5year old daughter. Using her ideas I created a 'Random Number Generator' game to help her practice numbers from 0-100. She helped me choose the gif animations as well suggest we add a visual mini-game such as the tower jumble game.",
+        url: "https://random-number-generator-8ca.pages.dev/"},
         ]
+
+        function goToSite(url) {
+            window.open(url, '_blank')
+        }
 
         const projectsElements = projectsArr.map( (project) =>
             <div key={project.title} className="project-box" >
@@ -30,7 +37,10 @@ export default function Projects(){
                 </div>
                 <div className="project-text">
                     <p className="project-title">{project.title}</p>
-                    <div className="project-tag">{project.tag}</div>
+                    <div className="tag-container">
+                        <div className="project-tag">{project.tag}</div>
+                            <button className="project-btn" onClick={()=>goToSite(project.url)}>Visit Project</button>
+                    </div>
                     <p className="project-desc greyTextLight">{project.description}</p>
                 </div>
                 
